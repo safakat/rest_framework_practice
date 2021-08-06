046902981381b4697612923802d257aef5751ef7
Safakat Rahman <safakat001@gmail.com>
2021-08-06 13:34:39 +0530

    Crete profile model and add Profile model in admin

--- social/models.py
+++ social/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=25, null=False, blank=False)
+    full_name = models.CharField(max_length=100, null=False, blank=False)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
